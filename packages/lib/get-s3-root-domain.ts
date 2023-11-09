8dda9902ddc97cb6a658d01d345a60bceb3f61e8
aiconauser98 <143793754+aiconauser98@users.noreply.github.com>
2023-11-09 14:49:26 +0100

    Update get-s3-root-domain.ts

--- packages/lib/get-s3-root-domain.ts
+++ packages/lib/get-s3-root-domain.ts
@@ -2,7 +2,7 @@ const getS3RootDomain = () => {
   if (process.env.NEXT_PUBLIC_AWS_ENDPOINT) {
     return `${process.env.NEXT_PUBLIC_AWS_ENDPOINT}`;
   }
-  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.amazonaws.com`;
+  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.eu-central-1.com`;
 };
 
 export default getS3RootDomain;
