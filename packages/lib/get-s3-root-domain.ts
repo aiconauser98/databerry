60b2f7eb55f9b913b8660df464d290cd7e2ad694
aiconauser98 <143793754+aiconauser98@users.noreply.github.com>
2023-11-09 14:53:30 +0100

    Update get-s3-root-domain.ts

--- packages/lib/get-s3-root-domain.ts
+++ packages/lib/get-s3-root-domain.ts
@@ -2,7 +2,7 @@ const getS3RootDomain = () => {
   if (process.env.NEXT_PUBLIC_AWS_ENDPOINT) {
     return `${process.env.NEXT_PUBLIC_AWS_ENDPOINT}`;
   }
-  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.eu-central-1.com`;
+  return `https://ai-conatex.s3.eu-central-1.com`;
 };
 
 export default getS3RootDomain;
